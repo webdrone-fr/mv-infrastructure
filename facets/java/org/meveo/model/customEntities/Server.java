28a955696bbb15f348fe18b59a647d55f056bc64
meveo.admin <no-mail@meveo.com>
2022-01-17 01:05:41 +0000

    Add the cet java source file : Server.javain the module : infrastructure

---
+++ facets/java/org/meveo/model/customEntities/Server.java
@@ -0,0 +1,175 @@
+package org.meveo.model.customEntities;
+
+import org.meveo.model.CustomEntity;
+import java.util.List;
+import org.meveo.model.persistence.DBStorageType;
+import java.time.Instant;
+import org.meveo.model.customEntities.ServiceProvider;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class Server implements CustomEntity {
+
+    public Server() {
+    }
+
+    public Server(String uuid) {
+        this.uuid = uuid;
+    }
+
+    private String uuid;
+
+    @JsonIgnore()
+    private DBStorageType storages;
+
+    private String image;
+
+    private String instanceName;
+
+    private String sergentUrl;
+
+    private String publicIp;
+
+    private Instant creationDate;
+
+    private String volumeSize;
+
+    private ServiceProvider provider;
+
+    private String zone;
+
+    private String domainName;
+
+    private Instant lastUpdate;
+
+    private String organization;
+
+    private String serverType;
+
+    private String status;
+
+    @Override()
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public DBStorageType getStorages() {
+        return storages;
+    }
+
+    public void setStorages(DBStorageType storages) {
+        this.storages = storages;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getInstanceName() {
+        return instanceName;
+    }
+
+    public void setInstanceName(String instanceName) {
+        this.instanceName = instanceName;
+    }
+
+    public String getSergentUrl() {
+        return sergentUrl;
+    }
+
+    public void setSergentUrl(String sergentUrl) {
+        this.sergentUrl = sergentUrl;
+    }
+
+    public String getPublicIp() {
+        return publicIp;
+    }
+
+    public void setPublicIp(String publicIp) {
+        this.publicIp = publicIp;
+    }
+
+    public Instant getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Instant creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getVolumeSize() {
+        return volumeSize;
+    }
+
+    public void setVolumeSize(String volumeSize) {
+        this.volumeSize = volumeSize;
+    }
+
+    public ServiceProvider getProvider() {
+        return provider;
+    }
+
+    public void setProvider(ServiceProvider provider) {
+        this.provider = provider;
+    }
+
+    public String getZone() {
+        return zone;
+    }
+
+    public void setZone(String zone) {
+        this.zone = zone;
+    }
+
+    public String getDomainName() {
+        return domainName;
+    }
+
+    public void setDomainName(String domainName) {
+        this.domainName = domainName;
+    }
+
+    public Instant getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Instant lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+
+    public String getServerType() {
+        return serverType;
+    }
+
+    public void setServerType(String serverType) {
+        this.serverType = serverType;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override()
+    public String getCetCode() {
+        return "Server";
+    }
+}
