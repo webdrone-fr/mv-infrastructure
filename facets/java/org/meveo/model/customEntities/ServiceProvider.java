e300beeeeccbac34284973f65698d195509ba45e
meveo.admin <no-mail@meveo.com>
2022-01-17 10:34:52 +0000

    Add the cet java source file : ServiceProvider.javain the module : infrastructure

--- facets/java/org/meveo/model/customEntities/ServiceProvider.java
+++ facets/java/org/meveo/model/customEntities/ServiceProvider.java
@@ -3,6 +3,9 @@ package org.meveo.model.customEntities;
 import org.meveo.model.CustomEntity;
 import java.util.List;
 import org.meveo.model.persistence.DBStorageType;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class ServiceProvider implements CustomEntity {
@@ -19,6 +22,22 @@ public class ServiceProvider implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
+    private String apiBaseUrl;
+
+    private Map<String, String> image = new HashMap<>();
+
+    private String code;
+
+    private Map<String, String> zone = new HashMap<>();
+
+    private Map<String, String> organization = new HashMap<>();
+
+    private Map<String, String> serverType = new HashMap<>();
+
+    private String description;
+
+    private List<String> status = new ArrayList<>();
+
     @Override()
     public String getUuid() {
         return uuid;
@@ -34,6 +53,70 @@ public class ServiceProvider implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
+    }
+
+    public String getApiBaseUrl() {
+        return apiBaseUrl;
+    }
+
+    public void setApiBaseUrl(String apiBaseUrl) {
+        this.apiBaseUrl = apiBaseUrl;
+    }
+
+    public Map<String, String> getImage() {
+        return image;
+    }
+
+    public void setImage(Map<String, String> image) {
+        this.image = image;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Map<String, String> getZone() {
+        return zone;
+    }
+
+    public void setZone(Map<String, String> zone) {
+        this.zone = zone;
+    }
+
+    public Map<String, String> getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(Map<String, String> organization) {
+        this.organization = organization;
+    }
+
+    public Map<String, String> getServerType() {
+        return serverType;
+    }
+
+    public void setServerType(Map<String, String> serverType) {
+        this.serverType = serverType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getStatus() {
+        return status;
+    }
+
+    public void setStatus(List<String> status) {
+        this.status = status;
     }
 
     @Override()
