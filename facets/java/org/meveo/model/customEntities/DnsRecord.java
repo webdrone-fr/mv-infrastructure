6219e964f84961fd49c50d77be4e7e824536a81e
meveo.admin <no-mail@meveo.com>
2022-01-17 01:05:43 +0000

    Add the cet java source file : DnsRecord.javain the module : infrastructure

---
+++ facets/java/org/meveo/model/customEntities/DnsRecord.java
@@ -0,0 +1,105 @@
+package org.meveo.model.customEntities;
+
+import org.meveo.model.CustomEntity;
+import java.util.List;
+import org.meveo.model.persistence.DBStorageType;
+import org.meveo.model.customEntities.DomainName;
+import java.time.Instant;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class DnsRecord implements CustomEntity {
+
+    public DnsRecord() {
+    }
+
+    public DnsRecord(String uuid) {
+        this.uuid = uuid;
+    }
+
+    private String uuid;
+
+    @JsonIgnore()
+    private DBStorageType storages;
+
+    private String recordType;
+
+    private DomainName domainName;
+
+    private String name;
+
+    private Instant lastSyncDate;
+
+    private Long ttl;
+
+    private String value;
+
+    @Override()
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public DBStorageType getStorages() {
+        return storages;
+    }
+
+    public void setStorages(DBStorageType storages) {
+        this.storages = storages;
+    }
+
+    public String getRecordType() {
+        return recordType;
+    }
+
+    public void setRecordType(String recordType) {
+        this.recordType = recordType;
+    }
+
+    public DomainName getDomainName() {
+        return domainName;
+    }
+
+    public void setDomainName(DomainName domainName) {
+        this.domainName = domainName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Instant getLastSyncDate() {
+        return lastSyncDate;
+    }
+
+    public void setLastSyncDate(Instant lastSyncDate) {
+        this.lastSyncDate = lastSyncDate;
+    }
+
+    public Long getTtl() {
+        return ttl;
+    }
+
+    public void setTtl(Long ttl) {
+        this.ttl = ttl;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override()
+    public String getCetCode() {
+        return "DnsRecord";
+    }
+}
