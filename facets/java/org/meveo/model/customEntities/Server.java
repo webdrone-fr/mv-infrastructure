ce93fdac524a7c3a0a8834755e464a6f28ceb008
meveo.admin <no-mail@meveo.com>
2022-01-17 10:34:26 +0000

    Add the cet java source file : Server.javain the module : infrastructure

--- facets/java/org/meveo/model/customEntities/Server.java
+++ facets/java/org/meveo/model/customEntities/Server.java
@@ -3,8 +3,6 @@ package org.meveo.model.customEntities;
 import org.meveo.model.CustomEntity;
 import java.util.List;
 import org.meveo.model.persistence.DBStorageType;
-import java.time.Instant;
-import org.meveo.model.customEntities.ServiceProvider;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class Server implements CustomEntity {
@@ -21,32 +19,6 @@ public class Server implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
-    private String image;
-
-    private String instanceName;
-
-    private String sergentUrl;
-
-    private String publicIp;
-
-    private Instant creationDate;
-
-    private String volumeSize;
-
-    private ServiceProvider provider;
-
-    private String zone;
-
-    private String domainName;
-
-    private Instant lastUpdate;
-
-    private String organization;
-
-    private String serverType;
-
-    private String status;
-
     @Override()
     public String getUuid() {
         return uuid;
@@ -62,110 +34,6 @@ public class Server implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public String getInstanceName() {
-        return instanceName;
-    }
-
-    public void setInstanceName(String instanceName) {
-        this.instanceName = instanceName;
-    }
-
-    public String getSergentUrl() {
-        return sergentUrl;
-    }
-
-    public void setSergentUrl(String sergentUrl) {
-        this.sergentUrl = sergentUrl;
-    }
-
-    public String getPublicIp() {
-        return publicIp;
-    }
-
-    public void setPublicIp(String publicIp) {
-        this.publicIp = publicIp;
-    }
-
-    public Instant getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(Instant creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    public String getVolumeSize() {
-        return volumeSize;
-    }
-
-    public void setVolumeSize(String volumeSize) {
-        this.volumeSize = volumeSize;
-    }
-
-    public ServiceProvider getProvider() {
-        return provider;
-    }
-
-    public void setProvider(ServiceProvider provider) {
-        this.provider = provider;
-    }
-
-    public String getZone() {
-        return zone;
-    }
-
-    public void setZone(String zone) {
-        this.zone = zone;
-    }
-
-    public String getDomainName() {
-        return domainName;
-    }
-
-    public void setDomainName(String domainName) {
-        this.domainName = domainName;
-    }
-
-    public Instant getLastUpdate() {
-        return lastUpdate;
-    }
-
-    public void setLastUpdate(Instant lastUpdate) {
-        this.lastUpdate = lastUpdate;
-    }
-
-    public String getOrganization() {
-        return organization;
-    }
-
-    public void setOrganization(String organization) {
-        this.organization = organization;
-    }
-
-    public String getServerType() {
-        return serverType;
-    }
-
-    public void setServerType(String serverType) {
-        this.serverType = serverType;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
     }
 
     @Override()
