f051f6b8be6c31080be3e8e2a59b39adb0bcecd9
meveo.admin <clement.bareth@web-drone.fr>
2022-01-26 09:29:09 +0100

    Add the cet java source file : ApplicationInstance.javain the module : Infrastructure

--- facets/java/org/meveo/model/customEntities/ApplicationInstance.java
+++ facets/java/org/meveo/model/customEntities/ApplicationInstance.java
@@ -19,6 +19,8 @@ public class ApplicationInstance implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
+    private String application;
+
     @Override()
     public String getUuid() {
         return uuid;
@@ -34,6 +36,14 @@ public class ApplicationInstance implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
+    }
+
+    public String getApplication() {
+        return application;
+    }
+
+    public void setApplication(String application) {
+        this.application = application;
     }
 
     @Override()
