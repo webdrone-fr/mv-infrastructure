c5f4ac9d045141910a9adb7b8afcb4b6fcc2ef6f
meveo.admin <clement.bareth@web-drone.fr>
2022-01-26 09:29:43 +0100

    Add the cet java source file : ServiceProvider.javain the module : Infrastructure

--- facets/java/org/meveo/model/customEntities/ServiceProvider.java
+++ facets/java/org/meveo/model/customEntities/ServiceProvider.java
@@ -29,6 +29,8 @@ public class ServiceProvider implements CustomEntity {
 
     private Map<String, String> organization = new HashMap<>();
 
+    private Map<String, String> serverType = new HashMap<>();
+
     private String description;
 
     @Override()
@@ -78,6 +80,14 @@ public class ServiceProvider implements CustomEntity {
 
     public void setOrganization(Map<String, String> organization) {
         this.organization = organization;
+    }
+
+    public Map<String, String> getServerType() {
+        return serverType;
+    }
+
+    public void setServerType(Map<String, String> serverType) {
+        this.serverType = serverType;
     }
 
     public String getDescription() {
