9fd397d9cd6ab7ddf9d3997e3d77cb70cdb4fee6
meveo.admin <clement.bareth@web-drone.fr>
2022-01-26 09:29:04 +0100

    Add the cet java source file : ServerAction.javain the module : Infrastructure

--- facets/java/org/meveo/model/customEntities/ServerAction.java
+++ facets/java/org/meveo/model/customEntities/ServerAction.java
@@ -3,8 +3,6 @@ package org.meveo.model.customEntities;
 import org.meveo.model.CustomEntity;
 import java.util.List;
 import org.meveo.model.persistence.DBStorageType;
-import org.meveo.model.customEntities.Server;
-import java.time.Instant;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class ServerAction implements CustomEntity {
@@ -21,18 +19,6 @@ public class ServerAction implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
-    private Server server;
-
-    private String response;
-
-    private String action;
-
-    private Instant creationDate;
-
-    private String responseStatus;
-
-    private Long elapsedTimeMs;
-
     @Override()
     public String getUuid() {
         return uuid;
@@ -48,54 +34,6 @@ public class ServerAction implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
-    }
-
-    public Server getServer() {
-        return server;
-    }
-
-    public void setServer(Server server) {
-        this.server = server;
-    }
-
-    public String getResponse() {
-        return response;
-    }
-
-    public void setResponse(String response) {
-        this.response = response;
-    }
-
-    public String getAction() {
-        return action;
-    }
-
-    public void setAction(String action) {
-        this.action = action;
-    }
-
-    public Instant getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(Instant creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    public String getResponseStatus() {
-        return responseStatus;
-    }
-
-    public void setResponseStatus(String responseStatus) {
-        this.responseStatus = responseStatus;
-    }
-
-    public Long getElapsedTimeMs() {
-        return elapsedTimeMs;
-    }
-
-    public void setElapsedTimeMs(Long elapsedTimeMs) {
-        this.elapsedTimeMs = elapsedTimeMs;
     }
 
     @Override()
