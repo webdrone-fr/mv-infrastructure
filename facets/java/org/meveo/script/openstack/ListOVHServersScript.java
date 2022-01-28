57aa2d8d45a94e07b72ed57f83fd4c6c40686f88
meveo.admin <clement.bareth@web-drone.fr>
2022-01-28 11:17:35 +0100

    Add the script File : org.meveo.script.openstack.ListOVHServersScriptin the module : Infrastructure

--- facets/java/org/meveo/script/openstack/ListOVHServersScript.java
+++ facets/java/org/meveo/script/openstack/ListOVHServersScript.java
@@ -78,15 +78,15 @@ public class ListOVHServersScript extends Script {
             HashMap<Object, Object> user = new HashMap<Object, Object>();
             HashMap<Object, Object> domain = new HashMap<Object, Object>();
             ArrayList <String> method = new ArrayList<String>();
-            method.add("password");
-            domain.put("id", "default");
-            user.put("password", "password");
-            user.put("domain", domain);
-            user.put("name", "user-4J6N43NBW3ch");
-            password.put("user", user);
-            identity.put("password", password);
-            identity.put("methods", method);
-            auth.put("identity", identity);
+            //method.add("password");
+            //domain.put("id", "default");
+            //user.put("password", "password");
+            //user.put("domain", domain);
+            //user.put("name", "user-4J6N43NBW3ch");
+            //password.put("user", user);
+            //identity.put("password", password);
+            //identity.put("methods", method);
+            auth.put("identity", "cc");
             master.put("auth", auth);
             String resp = JacksonUtil.toStringPrettyPrinted(master);
             // Creation of the identity token
