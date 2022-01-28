e6018da92abf52d5ce833b47311730b70bb5b221
meveo.admin <clement.bareth@web-drone.fr>
2022-01-28 14:45:53 +0100

    Add the script File : org.meveo.script.openstack.ListOVHServersScriptin the module : Infrastructure

--- facets/java/org/meveo/script/openstack/ListOVHServersScript.java
+++ facets/java/org/meveo/script/openstack/ListOVHServersScript.java
@@ -93,7 +93,7 @@ public class ListOVHServersScript extends Script {
             WebTarget target = client.target("https://auth." + openstack.getApiBaseUrl() + "/v3/auth/tokens");
         	Response response = target.request().post(Entity.json(resp));
             String value = response.readEntity(String.class);
-            log.info("Bonjour voici LA REPONSE !!!! => " + value);
+            log.info("Bonjour voici LA REPONSE !!!! => " + response.toString());
             if (response.getStatus() < 300) {
                 JsonArray rootArray = new JsonParser().parse(value).getAsJsonObject().getAsJsonArray("Headers");
                 for (JsonElement element : rootArray) {
