5e252f205de3a008094ce539970899ffab614ae2
meveo.admin <clement.bareth@web-drone.fr>
2022-02-01 10:26:30 +0100

    Add the script File : org.meveo.script.openstack.ListOVHServersScriptin the module : Infrastructure

--- facets/java/org/meveo/script/openstack/ListOVHServersScript.java
+++ facets/java/org/meveo/script/openstack/ListOVHServersScript.java
@@ -126,11 +126,10 @@ public class ListOVHServersScript extends Script {
                     server.setOrganization(serverObj.get("tenant_id").getAsString());
                     //image
                     String idImage = serverObj.get("image").getAsJsonObject().get("id").getAsString();
-                    log.info("id image : " + idImage);
-                    log.info("id server : " + serverObj.get("id").getAsString());
                   	WebTarget targetImage = clientListServers.target("https://compute." + zone + "." + openstack.getApiBaseUrl() + "/v2.1/images/"+idImage);
                     Response responseImage = targetImage.request().header("X-Auth-Token", credential.getToken()).get();
                     String ImageValue = responseImage.readEntity(String.class);
+                    log.info(String.valueOf(response.getStatus()));
                     if (response.getStatus() < 300) {
                       	JsonParser parser = new JsonParser();
                         JsonElement jsonE = parser.parse(ImageValue);
