23917e2301e3ecd026da5b7f6d61dc204c99c04a
meveo.admin <clement.bareth@web-drone.fr>
2022-02-04 11:22:43 +0100

    Add the script File : org.meveo.script.openstack.CheckOVHTokenin the module : Infrastructure

---
+++ facets/java/org/meveo/script/openstack/CheckOVHToken.java
@@ -0,0 +1,79 @@
+package org.meveo.script.openstack;
+
+import java.util.Map;
+import org.meveo.service.script.Script;
+import org.meveo.admin.exception.BusinessException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.meveo.model.customEntities.ServiceProvider;
+import org.meveo.model.customEntities.Credential;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.time.OffsetDateTime;
+import org.meveo.model.persistence.JacksonUtil;
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.*;
+import org.meveo.api.persistence.CrossStorageApi;
+import org.meveo.model.storage.Repository;
+import org.meveo.service.storage.RepositoryService;
+
+
+public class CheckOVHToken extends Script {
+
+    private static final Logger log = LoggerFactory.getLogger(ListOVHServersScript.class);
+  
+    private CrossStorageApi crossStorageApi = getCDIBean(CrossStorageApi.class);
+  
+    private RepositoryService repositoryService = getCDIBean(RepositoryService.class);
+  
+    private Repository defaultRepo = repositoryService.findDefaultRepository();
+
+    @Override
+    public void execute(Map<String, Object> parameters) throws BusinessException {
+        super.execute(parameters);
+    }
+
+    public void checkOVHToken(Credential credential, ServiceProvider openstack) {
+        log.info("call CheckOVHToken");
+        // Verification of the token
+        OffsetDateTime currentDate = OffsetDateTime.now();
+        OffsetDateTime expireDate = OffsetDateTime.parse(credential.getTokenExpiry().toString());
+        if (currentDate.isAfter(expireDate)) {
+            // Dechiffrement du mot de passe
+            // String stringToDecrypt = credential.getPasswordSecret();
+            // String hash = CEIUtils.getHash(null, null);
+            // String decryptedString = PasswordUtils.decrypt(salt, stringToDecrypt);
+            // Creation du body
+            HashMap<String, Object> master = new HashMap<String, Object>();
+            HashMap<String, Object> auth = new HashMap<String, Object>();
+            HashMap<String, Object> identity = new HashMap<String, Object>();
+            HashMap<String, Object> password = new HashMap<String, Object>();
+            HashMap<String, Object> user = new HashMap<String, Object>();
+            HashMap<String, Object> domain = new HashMap<String, Object>();
+            ArrayList<String> method = new ArrayList<String>();
+            method.add("password");
+            domain.put("id", "default");
+            user.put("password", "yjkhNrpjaWaYkGZYbs6z3gmDa5V74R9Z");
+            user.put("domain", domain);
+            user.put("name", credential.getUsername());
+            password.put("user", user);
+            identity.put("methods", method);
+            identity.put("password", password);
+            auth.put("identity", identity);
+            master.put("auth", auth);
+            String resp = JacksonUtil.toStringPrettyPrinted(master);
+            // Creation of the identity token
+            Client client = ClientBuilder.newClient();
+            WebTarget target = client.target("https://auth." + openstack.getApiBaseUrl() + "/v3/auth/tokens");
+            Response response = target.request().post(Entity.json(resp));
+            credential.setToken(response.getHeaderString("X-Subject-Token"));
+            credential.setTokenExpiry(currentDate.plusDays(1).toInstant());
+            try {
+                crossStorageApi.createOrUpdate(defaultRepo, credential);
+            } catch (Exception ex) {
+                log.error("error update credentials {} :{}", credential.getUuid(), ex.getMessage());
+            }
+            response.close();
+        }
+    }
+}
