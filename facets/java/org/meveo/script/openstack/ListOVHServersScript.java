6d4ccb38f96d1a2f001591f8f7a3fecacc5ab984
meveo.admin <clement.bareth@web-drone.fr>
2022-02-04 10:35:32 +0100

    Add the script File : org.meveo.script.openstack.ListOVHServersScriptin the module : Infrastructure

--- facets/java/org/meveo/script/openstack/ListOVHServersScript.java
+++ facets/java/org/meveo/script/openstack/ListOVHServersScript.java
@@ -95,12 +95,94 @@ public class ListOVHServersScript extends Script {
             credential.setToken(response.getHeaderString("X-Subject-Token"));
             credential.setTokenExpiry(currentDate.plusDays(1).toInstant());
             try {
-              crossStorageApi.createOrUpdate(defaultRepo, credential);
+                crossStorageApi.createOrUpdate(defaultRepo, credential);
             } catch (Exception ex) {
-              log.error("error update credentials {} :{}", credential.getUuid(), ex.getMessage());
+                log.error("error update credentials {} :{}", credential.getUuid(), ex.getMessage());
             }
             response.close();
         }
+        // Call every region to list server
+        Map<String, String> zones = new HashMap<String, String>();
+        zones = openstack.getZone();
+        for (String zone : zones.keySet()) {
+            Client clientListServers = ClientBuilder.newClient();
+            WebTarget targetListServer = clientListServers.target("https://compute." + zone + "." + openstack.getApiBaseUrl() + "/v2.1/servers/detail");
+            Response response = targetListServer.request().header("X-Auth-Token", credential.getToken()).get();
+            String value = response.readEntity(String.class);
+            if (response.getStatus() < 300) {
+                JsonArray rootArray = new JsonParser().parse(value).getAsJsonObject().getAsJsonArray("servers");
+                for (JsonElement element : rootArray) {
+                    JsonObject serverObj = element.getAsJsonObject();
+                    // Create new servers
+                    Server server = new Server();
+                    // UUID
+                    server.setUuid(serverObj.get("id").getAsString());
+                    // server name
+                    server.setInstanceName(serverObj.get("name").getAsString());
+                    // domain name
+                    server.setDomainName(serverObj.get("name").getAsString().toLowerCase() + ".webdrone.fr");
+                    // tenant
+                    server.setOrganization(serverObj.get("tenant_id").getAsString());
+                    // image
+                    String idImage = serverObj.get("image").getAsJsonObject().get("id").getAsString();
+                    WebTarget targetImage = clientListServers.target("https://image.compute." + zone + "." + openstack.getApiBaseUrl() + "/v2/images/" + idImage);
+                    Response responseImage = targetImage.request().header("X-Auth-Token", credential.getToken()).get();
+                    String ImageValue = responseImage.readEntity(String.class);
+                    if (!(ImageValue.startsWith("404"))) {
+                        JsonParser parser = new JsonParser();
+                        JsonElement jsonE = parser.parse(ImageValue);
+                        JsonObject ImageObj = jsonE.getAsJsonObject();
+                        if (ImageObj != null) {
+                            server.setImage(ImageObj.get("name").getAsString());
+                        }
+                    } else {
+                        server.setImage("Image not found");
+                        log.error("Image with id : " + idImage + " cannot be found for the server : " + serverObj.get("name").getAsString());
+                    }
+                    // Set the creation & updated date
+                    server.setCreationDate(OffsetDateTime.parse(serverObj.get("created").getAsString()).toInstant());
+                    server.setLastUpdate(OffsetDateTime.parse(serverObj.get("updated").getAsString()).toInstant());
+                    // zone
+                    server.setZone(zone);
+                    // public IP
+                    JsonArray publicIpArray = serverObj.get("addresses").getAsJsonObject().get("Ext-Net").getAsJsonArray();
+                    for (JsonElement ip : publicIpArray) {
+                        JsonObject ipElement = ip.getAsJsonObject();
+                        if (ipElement.get("version").getAsInt() == 4) {
+                            server.setPublicIp(ipElement.get("addr").getAsString());
+                        }
+                    }
+                    // status
+                    server.setStatus(serverObj.get("status").getAsString());
+                    // provider
+                    server.setProvider(openstack);
+                    // volume & flavor
+                    String idFlavor = serverObj.get("flavor").getAsJsonObject().get("id").getAsString();
+                    WebTarget targetVolume = clientListServers.target("https://compute." + zone + "." + openstack.getApiBaseUrl() + "/v2.1/flavors/" + idFlavor);
+                    Response responseVolume = targetVolume.request().header("X-Auth-Token", credential.getToken()).get();
+                    String flavorValue = responseVolume.readEntity(String.class);
+                    if (response.getStatus() < 300) {
+                        JsonParser parser = new JsonParser();
+                        JsonElement jsonE = parser.parse(flavorValue);
+                        JsonObject flavorObj = jsonE.getAsJsonObject();
+                        flavorObj = flavorObj.get("flavor").getAsJsonObject();
+                        // flavor
+                        server.setServerType(flavorObj.get("name").getAsString());
+                        // volume
+                        server.setVolumeSize(flavorObj.get("disk").getAsString() + " GiB");
+                    }
+                    try {
+                        crossStorageApi.createOrUpdate(defaultRepo, server);
+                    } catch (Exception ex) {
+                        log.error("error creating server {} :{}", server.getUuid(), ex.getMessage());
+                    }
+                }
+            }
+            response.close();
+        }
+    }
+
+    public void callOVH(Credential credential, ServiceProvider openstack) {
         // Call every region to list server
         Map<String, String> zones = new HashMap<String, String>();
         zones = openstack.getZone();
