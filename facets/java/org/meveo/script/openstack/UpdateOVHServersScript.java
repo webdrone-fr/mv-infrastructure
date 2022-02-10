35948109208190dec142a4868a7eada3f52bcf13
meveo.admin <clement.bareth@web-drone.fr>
2022-02-10 16:35:50 +0100

    Add the script File : org.meveo.script.openstack.UpdateOVHServersScriptin the module : Infrastructure

--- facets/java/org/meveo/script/openstack/UpdateOVHServersScript.java
+++ facets/java/org/meveo/script/openstack/UpdateOVHServersScript.java
@@ -53,10 +53,8 @@ public class UpdateOVHServersScript extends Script {
       	String codeClass = server.getClass().getSimpleName();
 		CustomEntityTemplate newServCET = customEntityTemplateService.findByCode(codeClass);
       	Map<String, CustomFieldTemplate> newServCFT = customFieldTemplateService.findByAppliesTo(newServCET.getAppliesTo());
-      	log.info("New values : " + newServCFT.toString());
-      	log.info("Old values : " + oldServ.toString());
       	for(Map.Entry<String, CustomFieldTemplate> entry : newServCFT.entrySet()) {
-          	
+			log.info(entry.toString());
         }
     }
 
