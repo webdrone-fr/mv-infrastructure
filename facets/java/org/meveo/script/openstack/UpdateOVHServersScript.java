a9863c181b345e18d95a215eb53f33c2335fa8a6
meveo.admin <clement.bareth@web-drone.fr>
2022-02-10 16:55:25 +0100

    Add the script File : org.meveo.script.openstack.UpdateOVHServersScriptin the module : Infrastructure

--- facets/java/org/meveo/script/openstack/UpdateOVHServersScript.java
+++ facets/java/org/meveo/script/openstack/UpdateOVHServersScript.java
@@ -59,6 +59,7 @@ public class UpdateOVHServersScript extends Script {
       	for(Map.Entry<String, CustomFieldTemplate> entry : newServCFT.entrySet()) {
 			log.info(entry.getKey());
           	Object oldValue = oldServ.get(entry.getKey());
+          	String newValue = newToCEI.get(entry.getKey());
         }
     }
 
