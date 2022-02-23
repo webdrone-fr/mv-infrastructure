294444f6c083382dc5df7bf898ac560404f4766f
meveo.admin <no-mail@meveo.com>
2022-02-23 10:36:37 +0000

    Add the cet java source file : DnsRecord.javain the module : mv-infrastructure

--- facets/java/org/meveo/model/customEntities/DnsRecord.java
+++ facets/java/org/meveo/model/customEntities/DnsRecord.java
@@ -3,8 +3,6 @@ package org.meveo.model.customEntities;
 import org.meveo.model.CustomEntity;
 import java.util.List;
 import org.meveo.model.persistence.DBStorageType;
-import org.meveo.model.customEntities.DomainName;
-import java.time.Instant;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class DnsRecord implements CustomEntity {
@@ -21,18 +19,6 @@ public class DnsRecord implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
-    private String recordType;
-
-    private DomainName domainName;
-
-    private String name;
-
-    private Instant lastSyncDate;
-
-    private Long ttl;
-
-    private String value;
-
     @Override()
     public String getUuid() {
         return uuid;
@@ -48,54 +34,6 @@ public class DnsRecord implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
-    }
-
-    public String getRecordType() {
-        return recordType;
-    }
-
-    public void setRecordType(String recordType) {
-        this.recordType = recordType;
-    }
-
-    public DomainName getDomainName() {
-        return domainName;
-    }
-
-    public void setDomainName(DomainName domainName) {
-        this.domainName = domainName;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Instant getLastSyncDate() {
-        return lastSyncDate;
-    }
-
-    public void setLastSyncDate(Instant lastSyncDate) {
-        this.lastSyncDate = lastSyncDate;
-    }
-
-    public Long getTtl() {
-        return ttl;
-    }
-
-    public void setTtl(Long ttl) {
-        this.ttl = ttl;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
     }
 
     @Override()
