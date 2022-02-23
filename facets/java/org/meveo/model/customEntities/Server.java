da013331b53964a640039e5e63ebffc842047512
meveo.admin <no-mail@meveo.com>
2022-02-23 13:20:57 +0000

    Add the cet java source file : Server.javain the module : mv-infrastructure

--- facets/java/org/meveo/model/customEntities/Server.java
+++ facets/java/org/meveo/model/customEntities/Server.java
@@ -25,8 +25,6 @@ public class Server implements CustomEntity {
 
     private String instanceName;
 
-    private String keyName;
-
     private String sergentUrl;
 
     private String publicIp;
@@ -80,14 +78,6 @@ public class Server implements CustomEntity {
 
     public void setInstanceName(String instanceName) {
         this.instanceName = instanceName;
-    }
-
-    public String getKeyName() {
-        return keyName;
-    }
-
-    public void setKeyName(String keyName) {
-        this.keyName = keyName;
     }
 
     public String getSergentUrl() {
