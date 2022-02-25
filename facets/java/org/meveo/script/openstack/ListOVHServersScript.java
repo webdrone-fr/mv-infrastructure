3859452cc77efbe2f2d14b6c8a32fe81e8360f35
meveo.admin <no-mail@meveo.com>
2022-02-25 14:49:27 +0000

    Add the script File : org.meveo.script.openstack.ListOVHServersScriptin the module : mv-infrastructure

--- facets/java/org/meveo/script/openstack/ListOVHServersScript.java
+++ facets/java/org/meveo/script/openstack/ListOVHServersScript.java
@@ -32,10 +32,10 @@ public class ListOVHServersScript extends Script {
     private RepositoryService repositoryService = getCDIBean(RepositoryService.class);
 
     private Repository defaultRepo = repositoryService.findDefaultRepository();
-  
+
     private CheckOVHToken checkOVHToken = new CheckOVHToken();
-  
-  	private OpenstackAPI openstackAPI = new OpenstackAPI();
+
+    private OpenstackAPI openstackAPI = new OpenstackAPI();
 
     @Override
     public void execute(Map<String, Object> parameters) throws BusinessException {
@@ -44,64 +44,64 @@ public class ListOVHServersScript extends Script {
 
     public void callOVH(Credential credential, ServiceProvider openstack) {
         log.info("calling ListOVHServersScript");
-        //Check the token
+        // Check the token
         checkOVHToken.checkOVHToken(credential, openstack);
         // Call every region to list server
         Map<String, String> zones = new HashMap<String, String>();
         zones = openstack.getZone();
         for (String zone : zones.keySet()) {
-          	List<JsonObject> servers = openstackAPI.computeAPI("servers/detail", credential.getToken(), null);
-                for (JsonElement element : servers) {
-                    JsonObject serverObj = element.getAsJsonObject();
-                    // Create new servers
-                    Server server = new Server();
-                    // UUID
-                    server.setUuid(serverObj.get("id").getAsString());
-                    // server name
-                    server.setInstanceName(serverObj.get("name").getAsString());
-                    // domain name
-                    server.setDomainName(serverObj.get("name").getAsString().toLowerCase() + ".webdrone.fr");
-                    // tenant
-                    server.setOrganization(serverObj.get("tenant_id").getAsString());
-                    // image
-                    String idImage = serverObj.get("image").getAsJsonObject().get("id").getAsString();
-                  	String urlImage = "images/" + idImage;
-                  	log.info(urlImage);
-                  	List<JsonObject> images = openstackAPI.computeAPI(urlImage, credential.getToken(), null);
-                  	for (JsonObject imageElement : images) {
-                      	server.setImage(imageElement.get("name").getAsString());
-                    }
-                    // Set the creation & updated date
-                    server.setCreationDate(OffsetDateTime.parse(serverObj.get("created").getAsString()).toInstant());
-                    server.setLastUpdate(OffsetDateTime.parse(serverObj.get("updated").getAsString()).toInstant());
-                    // zone
-                    server.setZone(zone);
-                    // public IP
-                    JsonArray publicIpArray = serverObj.get("addresses").getAsJsonObject().get("Ext-Net").getAsJsonArray();
-                    for (JsonElement ip : publicIpArray) {
-                        JsonObject ipElement = ip.getAsJsonObject();
-                        if (ipElement.get("version").getAsInt() == 4) {
-                            server.setPublicIp(ipElement.get("addr").getAsString());
-                        }
-                    }
-                    // status
-                    server.setStatus(serverObj.get("status").getAsString());
-                    // provider
-                    server.setProvider(openstack);
-                    // volume & flavor
-                    String idFlavor = serverObj.get("flavor").getAsJsonObject().get("id").getAsString();
-                  	String urlFlavor = "flavors/" + idFlavor;
-                  	List<JsonObject> flavors = openstackAPI.computeAPI(urlFlavor, credential.getToken(), null);
-                  	for (JsonObject flavor : flavors) {
-                      	server.setServerType(flavor.get("name").getAsString());
-                      	server.setVolumeSize(flavor.get("disk").getAsString() + " GiB");
-                    }
-                    try {
-                        crossStorageApi.createOrUpdate(defaultRepo, server);
-                    } catch (Exception ex) {
-                        log.error("error creating server {} :{}", server.getUuid(), ex.getMessage());
+            List<JsonObject> servers = openstackAPI.computeAPI("servers/detail", credential.getToken(), null);
+            for (JsonElement element : servers) {
+                JsonObject serverObj = element.getAsJsonObject();
+                // Create new servers
+                Server server = new Server();
+                // UUID
+                server.setUuid(serverObj.get("id").getAsString());
+                // server name
+                server.setInstanceName(serverObj.get("name").getAsString());
+                // domain name
+                server.setDomainName(serverObj.get("name").getAsString().toLowerCase() + ".webdrone.fr");
+                // tenant
+                server.setOrganization(serverObj.get("tenant_id").getAsString());
+                // image
+                String idImage = serverObj.get("image").getAsJsonObject().get("id").getAsString();
+                String urlImage = "images/" + idImage;
+                log.info(urlImage);
+                List<JsonObject> images = openstackAPI.computeAPI(urlImage, credential.getToken(), null);
+                for (JsonObject imageElement : images) {
+                    server.setImage(imageElement.get("name").getAsString());
+                }
+                // Set the creation & updated date
+                server.setCreationDate(OffsetDateTime.parse(serverObj.get("created").getAsString()).toInstant());
+                server.setLastUpdate(OffsetDateTime.parse(serverObj.get("updated").getAsString()).toInstant());
+                // zone
+                server.setZone(zone);
+                // public IP
+                JsonArray publicIpArray = serverObj.get("addresses").getAsJsonObject().get("Ext-Net").getAsJsonArray();
+                for (JsonElement ip : publicIpArray) {
+                    JsonObject ipElement = ip.getAsJsonObject();
+                    if (ipElement.get("version").getAsInt() == 4) {
+                        server.setPublicIp(ipElement.get("addr").getAsString());
                     }
                 }
+                // status
+                server.setStatus(serverObj.get("status").getAsString());
+                // provider
+                server.setProvider(openstack);
+                // volume & flavor
+                String idFlavor = serverObj.get("flavor").getAsJsonObject().get("id").getAsString();
+                String urlFlavor = "flavors/" + idFlavor;
+                List<JsonObject> flavors = openstackAPI.computeAPI(urlFlavor, credential.getToken(), null);
+                for (JsonObject flavor : flavors) {
+                    server.setServerType(flavor.get("name").getAsString());
+                    server.setVolumeSize(flavor.get("disk").getAsString() + " GiB");
+                }
+                try {
+                    crossStorageApi.createOrUpdate(defaultRepo, server);
+                } catch (Exception ex) {
+                    log.error("error creating server {} :{}", server.getUuid(), ex.getMessage());
+                }
+            }
         }
     }
 }
