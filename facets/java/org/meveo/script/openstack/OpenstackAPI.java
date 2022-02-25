073488b6ccba419e6670281baacbdfea87806cf9
meveo.admin <no-mail@meveo.com>
2022-02-25 15:23:40 +0000

    Add the script File : org.meveo.script.openstack.OpenstackAPIin the module : mv-infrastructure

--- facets/java/org/meveo/script/openstack/OpenstackAPI.java
+++ facets/java/org/meveo/script/openstack/OpenstackAPI.java
@@ -31,8 +31,6 @@ public class OpenstackAPI extends Script {
             WebTarget target = client.target(this.computeBaseAPI + url);
             Response response = target.request().header("X-Auth-Token", token).get();
             String value = response.readEntity(String.class);
-                //log.info(url);
-                //log.info(String.valueOf(response.getStatus()));
             if (response.getStatus() < 300) {
                 String objectReturned = url.substring(0, url.indexOf("/"));
                 JsonArray rootArray = new JsonParser().parse(value).getAsJsonObject().getAsJsonArray(objectReturned);
@@ -46,8 +44,6 @@ public class OpenstackAPI extends Script {
             WebTarget target = client.target(this.computeBaseAPI + url);
             Response response = target.request().header("X-Auth-Token", token).post(Entity.json(jsonBody));
             String value = response.readEntity(String.class);
-                //log.info(url);
-                //log.info(String.valueOf(response.getStatus()));
             if (response.getStatus() < 300) {
                 String objectReturned = url.substring(0, url.indexOf("/"));
                 JsonArray rootArray = new JsonParser().parse(value).getAsJsonObject().getAsJsonArray(objectReturned);
