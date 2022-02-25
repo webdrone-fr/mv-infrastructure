3f2e6ffdd5f8bc64bd83a8d65865b6a4d2c79d6e
meveo.admin <no-mail@meveo.com>
2022-02-25 13:57:08 +0000

    Add the script File : org.meveo.script.openstack.OpenstackAPIin the module : mv-infrastructure

--- facets/java/org/meveo/script/openstack/OpenstackAPI.java
+++ facets/java/org/meveo/script/openstack/OpenstackAPI.java
@@ -41,8 +41,18 @@ public class OpenstackAPI extends Script {
   
   	public List<JsonObject> networkAPI(String url, String token, String jsonBody) {
       	List<JsonObject> res = new ArrayList<>();
-      	
-      	
+		Client client = ClientBuilder.newClient();
+      	WebTarget target = client.target(this.networkBaseAPI + url);
+      	Response response = target.request().header("X-Auth-Token", token).get();
+      	String value = response.readEntity(String.class);
+      	if (response.getStatus() < 300) {
+          	String objectReturned = url.substring(0, url.indexOf("/"));
+          	JsonArray rootArray = new JsonParser().parse(value).getAsJsonObject().getAsJsonArray(objectReturned);
+            for (JsonElement element : rootArray) {
+            	JsonObject JObject = element.getAsJsonObject();
+              	res.add(JObject);
+            }
+        }
       	return res;
     }
   
