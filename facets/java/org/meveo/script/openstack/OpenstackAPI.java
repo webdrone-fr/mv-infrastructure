feccb9f19b9920d35095dac531eb2b82120be5a2
meveo.admin <no-mail@meveo.com>
2022-02-25 13:58:18 +0000

    Add the script File : org.meveo.script.openstack.OpenstackAPIin the module : mv-infrastructure

--- facets/java/org/meveo/script/openstack/OpenstackAPI.java
+++ facets/java/org/meveo/script/openstack/OpenstackAPI.java
@@ -60,15 +60,35 @@ public class OpenstackAPI extends Script {
   
   	public List<JsonObject> imageAPI(String url, String token, String jsonBody) {
       	List<JsonObject> res = new ArrayList<>();
-      	
-      	
+		Client client = ClientBuilder.newClient();
+      	WebTarget target = client.target(this.imageBaseAPI + url);
+      	Response response = target.request().header("X-Auth-Token", token).get();
+      	String value = response.readEntity(String.class);
+      	if (response.getStatus() < 300) {
+          	String objectReturned = url.substring(0, url.indexOf("/"));
+          	JsonArray rootArray = new JsonParser().parse(value).getAsJsonObject().getAsJsonArray(objectReturned);
+            for (JsonElement element : rootArray) {
+            	JsonObject JObject = element.getAsJsonObject();
+              	res.add(JObject);
+            }
+        }
       	return res;
     }
   
   	public List<JsonObject> IdentityAPI(String url, String token, String jsonBody) {
       	List<JsonObject> res = new ArrayList<>();
-      	
-      	
+		Client client = ClientBuilder.newClient();
+      	WebTarget target = client.target(this.identityBaseAPI + url);
+      	Response response = target.request().header("X-Auth-Token", token).get();
+      	String value = response.readEntity(String.class);
+      	if (response.getStatus() < 300) {
+          	String objectReturned = url.substring(0, url.indexOf("/"));
+          	JsonArray rootArray = new JsonParser().parse(value).getAsJsonObject().getAsJsonArray(objectReturned);
+            for (JsonElement element : rootArray) {
+            	JsonObject JObject = element.getAsJsonObject();
+              	res.add(JObject);
+            }
+        }
       	return res;
     }
 	
