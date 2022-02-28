eb543f780c840a09ef1dadaf668f7675ec892300
meveo.admin <no-mail@meveo.com>
2022-02-28 09:32:48 +0000

    Add the script File : org.meveo.script.openstack.OpenstackAPIin the module : mv-infrastructure

--- facets/java/org/meveo/script/openstack/OpenstackAPI.java
+++ facets/java/org/meveo/script/openstack/OpenstackAPI.java
@@ -51,6 +51,7 @@ public class OpenstackAPI extends Script {
             }
             response.close();
         } else if (methodType.equalsIgnoreCase("post")) {
+          	log.info("EXECUTION DE LA REQUETE");
             WebTarget target = client.target(this.computeBaseAPI + url);
             Response response = target.request().header("X-Auth-Token", token).post(Entity.json(jsonBody));
             String value = response.readEntity(String.class);
