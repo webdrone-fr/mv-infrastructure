7ced10e65f67e95298ca834ca011eff47865d2a0
meveo.admin <no-mail@meveo.com>
2022-02-28 16:20:18 +0000

    Add the script File : org.meveo.script.ListSecurityGroupsin the module : mv-infrastructure

---
+++ facets/java/org/meveo/script/ListSecurityGroups.java
@@ -0,0 +1,29 @@
+package org.meveo.script;
+
+import java.util.Map;
+
+import org.meveo.service.script.Script;
+import org.meveo.admin.exception.BusinessException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.meveo.api.persistence.CrossStorageApi;
+import org.meveo.service.storage.RepositoryService;
+import org.meveo.model.storage.Repository;
+import org.meveo.model.customEntities.ServiceProvider;
+
+public class ListSecurityGroups extends Script {
+
+    private static final Logger log = LoggerFactory.getLogger(ListServerImages.class);
+
+    private CrossStorageApi crossStorageApi = getCDIBean(CrossStorageApi.class);
+
+    private RepositoryService repositoryService = getCDIBean(RepositoryService.class);
+
+    private Repository defaultRepo = repositoryService.findDefaultRepository();
+	
+	@Override
+	public void execute(Map<String, Object> parameters) throws BusinessException {
+		super.execute(parameters);
+	}
+	
+}
