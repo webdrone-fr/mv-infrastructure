db45b9237df41c6de0bb4554eae981eb2b1e9d4a
meveo.admin <no-mail@meveo.com>
2022-02-28 10:17:08 +0000

    Add the script File : org.meveo.script.openstack.OpenstackAPIin the module : mv-infrastructure

--- facets/java/org/meveo/script/openstack/OpenstackAPI.java
+++ facets/java/org/meveo/script/openstack/OpenstackAPI.java
@@ -1,7 +1,6 @@
 package org.meveo.script.openstack;
 
 import java.util.Map;
-
 import org.meveo.service.script.Script;
 import org.meveo.admin.exception.BusinessException;
 import org.slf4j.Logger;
@@ -16,16 +15,16 @@ import org.meveo.model.customEntities.Credential;
 public class OpenstackAPI extends Script {
 
     private static final Logger log = LoggerFactory.getLogger(ListOVHServersScript.class);
-  
-  	private String computeBaseAPI = "https://compute.gra11.cloud.ovh.net/v2.1/";
-  
-  	private String networkBaseAPI = "https://network.compute.gra11.cloud.ovh.net/v2.0/";
-  
-  	private String imageBaseAPI = "https://image.compute.gra11.cloud.ovh.net/v2/";
-  
-  	private String identityBaseAPI = "https://auth.cloud.ovh.net/";
-  
-  	/**
+
+    private String computeBaseAPI = "https://compute.gra11.cloud.ovh.net/v2.1/";
+
+    private String networkBaseAPI = "https://network.compute.gra11.cloud.ovh.net/v2.0/";
+
+    private String imageBaseAPI = "https://image.compute.gra11.cloud.ovh.net/v2/";
+
+    private String identityBaseAPI = "https://auth.cloud.ovh.net/";
+
+    /**
      * Do all of compute api call
      * @param url the path of the call
      * @param token the token currently used by the api
@@ -35,21 +34,26 @@ public class OpenstackAPI extends Script {
      * @return the list of json object
      * @throws if the methodType used is not supported
      */
-  	public List<JsonObject> computeAPI(String url, Credential token, String jsonBody, String methodType, String objReturn) throws BusinessException {
-      	List<JsonObject> res = new ArrayList<>();
+    public List<JsonObject> computeAPI(String url, Credential token, String jsonBody, String methodType, String objReturn) throws BusinessException {
+        List<JsonObject> res = new ArrayList<>();
         Client client = ClientBuilder.newClient();
-      	if (methodType.equalsIgnoreCase("get")) {
+        if (methodType.equalsIgnoreCase("get")) {
             WebTarget target = client.target(this.computeBaseAPI + url);
             Response response = target.request().header("X-Auth-Token", token.getToken()).get();
             String value = response.readEntity(String.class);
             if (response.getStatus() < 300) {
-              	log.info("LISTING DU SERVER : " + value.toString());
-                JsonArray rootArray = new JsonParser().parse(value).getAsJsonObject().getAsJsonArray(objReturn);
-                for (JsonElement element : rootArray) {
-                    JsonObject JObject = element.getAsJsonObject();
-                    res.add(JObject);
+              	String isList = objReturn + "s";
+                if (value.contains(isList)) {
+                    JsonArray rootArray = new JsonParser().parse(value).getAsJsonObject().getAsJsonArray(objReturn);
+                    for (JsonElement element : rootArray) {
+                        JsonObject JObject = element.getAsJsonObject();
+                        res.add(JObject);
+                    }
+                } else {
+                    JsonObject JObject = new JsonParser().parse(value).getAsJsonObject();
+                    JObject = JObject.get(objReturn).getAsJsonObject();
+                  	res.add(JObject);
                 }
-				log.info("REVOIE : " + res.toString());
             }
             response.close();
         } else if (methodType.equalsIgnoreCase("post")) {
@@ -57,19 +61,11 @@ public class OpenstackAPI extends Script {
             Response response = target.request().header("X-Auth-Token", token.getToken()).post(Entity.json(jsonBody));
             String value = response.readEntity(String.class);
             if (response.getStatus() < 300) {
-              	// Regarder si la reponse cotient <"servers": [>
-              	// Si oui JsonArray
-              	// Si non JsonObject direct
-              	//if (jp.parse(value).getAsJsonObject().getAsJsonArray(objReturn) instanceof JsonObject) {
                 JsonParser parserServer = new JsonParser();
                 JsonElement jsonServer = parserServer.parse(value);
                 JsonObject serverObj = jsonServer.getAsJsonObject();
                 serverObj = serverObj.get(objReturn).getAsJsonObject();
-              	res.add(serverObj);
-                //} else {
-                //  	JsonObject obj = new JsonParser().parse(value).getAsJsonObject();
-                //  	res.add(obj);
-                //}
+                res.add(serverObj);
             }
             response.close();
         } else if (methodType.equalsIgnoreCase("delete")) {
@@ -79,13 +75,13 @@ public class OpenstackAPI extends Script {
             response.close();
         } else if (methodType.equalsIgnoreCase("put")) {
         } else {
-          	throw new BusinessException("Cannot found " + methodType + " in method type request. Available methods : get, post, delete, put");
+            throw new BusinessException("Cannot found " + methodType + " in method type request. Available methods : get, post, delete, put");
         }
-		client.close();
-      	return res;
+        client.close();
+        return res;
     }
-  
-	/**
+
+    /**
      * Do all of network api call
      * @param url the path of the call
      * @param token the token currently used by the api
@@ -95,25 +91,25 @@ public class OpenstackAPI extends Script {
      * @return the list of json object
      * @throws if the methodType used is not supported
      */
-  	public List<JsonObject> networkAPI(String url, Credential token, String jsonBody, String methodType, String objReturn) {
-      	List<JsonObject> res = new ArrayList<>();
-		Client client = ClientBuilder.newClient();
-      	WebTarget target = client.target(this.networkBaseAPI + url);
-      	Response response = target.request().header("X-Auth-Token", token.getToken()).get();
-      	String value = response.readEntity(String.class);
-      	if (response.getStatus() < 300) {
-          	JsonArray rootArray = new JsonParser().parse(value).getAsJsonObject().getAsJsonArray(objReturn);
+    public List<JsonObject> networkAPI(String url, Credential token, String jsonBody, String methodType, String objReturn) {
+        List<JsonObject> res = new ArrayList<>();
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(this.networkBaseAPI + url);
+        Response response = target.request().header("X-Auth-Token", token.getToken()).get();
+        String value = response.readEntity(String.class);
+        if (response.getStatus() < 300) {
+            JsonArray rootArray = new JsonParser().parse(value).getAsJsonObject().getAsJsonArray(objReturn);
             for (JsonElement element : rootArray) {
-            	JsonObject JObject = element.getAsJsonObject();
-              	res.add(JObject);
+                JsonObject JObject = element.getAsJsonObject();
+                res.add(JObject);
             }
         }
-      	response.close();
-      	client.close();
-      	return res;
+        response.close();
+        client.close();
+        return res;
     }
-  
-	/**
+
+    /**
      * Do all of image api call
      * @param url the path of the call
      * @param token the token currently used by the api
@@ -123,25 +119,25 @@ public class OpenstackAPI extends Script {
      * @return the list of json object
      * @throws if the methodType used is not supported
      */
-  	public List<JsonObject> imageAPI(String url, Credential token, String jsonBody, String methodType, String objReturn) {
-      	List<JsonObject> res = new ArrayList<>();
-		Client client = ClientBuilder.newClient();
-      	WebTarget target = client.target(this.imageBaseAPI + url);
-      	Response response = target.request().header("X-Auth-Token", token.getToken()).get();
-      	String value = response.readEntity(String.class);
-      	if (response.getStatus() < 300) {
-          	JsonArray rootArray = new JsonParser().parse(value).getAsJsonObject().getAsJsonArray(objReturn);
+    public List<JsonObject> imageAPI(String url, Credential token, String jsonBody, String methodType, String objReturn) {
+        List<JsonObject> res = new ArrayList<>();
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(this.imageBaseAPI + url);
+        Response response = target.request().header("X-Auth-Token", token.getToken()).get();
+        String value = response.readEntity(String.class);
+        if (response.getStatus() < 300) {
+            JsonArray rootArray = new JsonParser().parse(value).getAsJsonObject().getAsJsonArray(objReturn);
             for (JsonElement element : rootArray) {
-            	JsonObject JObject = element.getAsJsonObject();
-              	res.add(JObject);
+                JsonObject JObject = element.getAsJsonObject();
+                res.add(JObject);
             }
         }
-      	response.close();
-      	client.close();
-      	return res;
+        response.close();
+        client.close();
+        return res;
     }
-  
-	/**
+
+    /**
      * Do all of identity api call
      * @param url the path of the call
      * @param token the token currently used by the api
@@ -151,28 +147,26 @@ public class OpenstackAPI extends Script {
      * @return the list of json object
      * @throws if the methodType used is not supported
      */
-  	public List<JsonObject> IdentityAPI(String url, Credential token, String jsonBody, String methodType, String objReturn) {
-      	List<JsonObject> res = new ArrayList<>();
-		Client client = ClientBuilder.newClient();
-      	WebTarget target = client.target(this.identityBaseAPI + url);
-      	Response response = target.request().header("X-Auth-Token", token.getToken()).get();
-      	String value = response.readEntity(String.class);
-      	if (response.getStatus() < 300) {
-          	JsonArray rootArray = new JsonParser().parse(value).getAsJsonObject().getAsJsonArray(objReturn);
+    public List<JsonObject> IdentityAPI(String url, Credential token, String jsonBody, String methodType, String objReturn) {
+        List<JsonObject> res = new ArrayList<>();
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(this.identityBaseAPI + url);
+        Response response = target.request().header("X-Auth-Token", token.getToken()).get();
+        String value = response.readEntity(String.class);
+        if (response.getStatus() < 300) {
+            JsonArray rootArray = new JsonParser().parse(value).getAsJsonObject().getAsJsonArray(objReturn);
             for (JsonElement element : rootArray) {
-            	JsonObject JObject = element.getAsJsonObject();
-              	res.add(JObject);
+                JsonObject JObject = element.getAsJsonObject();
+                res.add(JObject);
             }
         }
-      	response.close();
-      	client.close();
-      	return res;
+        response.close();
+        client.close();
+        return res;
     }
-	
-	@Override
-	public void execute(Map<String, Object> parameters) throws BusinessException {
-		super.execute(parameters);
-	}
-  
-	
+
+    @Override
+    public void execute(Map<String, Object> parameters) throws BusinessException {
+        super.execute(parameters);
+    }
 }
