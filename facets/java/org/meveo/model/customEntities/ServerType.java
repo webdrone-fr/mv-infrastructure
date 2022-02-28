4478ccb00a1086af00b7195a08d98c8c5c061b3b
meveo.admin <no-mail@meveo.com>
2022-02-28 14:34:40 +0000

    Add the cet java source file : ServerType.javain the module : mv-infrastructure

--- facets/java/org/meveo/model/customEntities/ServerType.java
+++ facets/java/org/meveo/model/customEntities/ServerType.java
@@ -3,7 +3,6 @@ package org.meveo.model.customEntities;
 import org.meveo.model.CustomEntity;
 import java.util.List;
 import org.meveo.model.persistence.DBStorageType;
-import org.meveo.model.customEntities.ServiceProvider;
 import java.time.Instant;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -20,8 +19,6 @@ public class ServerType implements CustomEntity {
 
     @JsonIgnore()
     private DBStorageType storages;
-
-    private ServiceProvider provider;
 
     private String zone;
 
@@ -48,14 +45,6 @@ public class ServerType implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
-    }
-
-    public ServiceProvider getProvider() {
-        return provider;
-    }
-
-    public void setProvider(ServiceProvider provider) {
-        this.provider = provider;
     }
 
     public String getZone() {
