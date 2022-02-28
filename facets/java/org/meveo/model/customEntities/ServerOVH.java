e241838f1c77720d002a9d78d88ac5d44e4fc005
meveo.admin <no-mail@meveo.com>
2022-02-28 14:27:20 +0000

    Add the cet java source file : ServerOVH.javain the module : mv-infrastructure

---
+++ facets/java/org/meveo/model/customEntities/ServerOVH.java
@@ -0,0 +1,44 @@
+package org.meveo.model.customEntities;
+
+import org.meveo.model.CustomEntity;
+import java.util.List;
+import org.meveo.model.persistence.DBStorageType;
+import org.meveo.model.customEntities.Server;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class ServerOVH extends Server implements CustomEntity {
+
+    public ServerOVH() {
+    }
+
+    public ServerOVH(String uuid) {
+        this.uuid = uuid;
+    }
+
+    private String uuid;
+
+    @JsonIgnore()
+    private DBStorageType storages;
+
+    @Override()
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public DBStorageType getStorages() {
+        return storages;
+    }
+
+    public void setStorages(DBStorageType storages) {
+        this.storages = storages;
+    }
+
+    @Override()
+    public String getCetCode() {
+        return "ServerOVH";
+    }
+}
