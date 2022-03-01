d25ac944f5a26ebcbffd1ee2ba8396a094de9b90
meveo.admin <no-mail@meveo.com>
2022-03-01 08:04:52 +0000

    Add the cet java source file : ServerFlavor.javain the module : mv-infrastructure

---
+++ facets/java/org/meveo/model/customEntities/ServerFlavor.java
@@ -0,0 +1,43 @@
+package org.meveo.model.customEntities;
+
+import org.meveo.model.CustomEntity;
+import java.util.List;
+import org.meveo.model.persistence.DBStorageType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class ServerFlavor implements CustomEntity {
+
+    public ServerFlavor() {
+    }
+
+    public ServerFlavor(String uuid) {
+        this.uuid = uuid;
+    }
+
+    private String uuid;
+
+    @JsonIgnore()
+    private DBStorageType storages;
+
+    @Override()
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public DBStorageType getStorages() {
+        return storages;
+    }
+
+    public void setStorages(DBStorageType storages) {
+        this.storages = storages;
+    }
+
+    @Override()
+    public String getCetCode() {
+        return "ServerFlavor";
+    }
+}
