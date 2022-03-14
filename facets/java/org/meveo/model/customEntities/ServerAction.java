732735510c9aa6f759ca8428789a87690fdfb306
meveo.admin <no-mail@meveo.com>
2022-03-14 10:15:41 +0000

    Add the cet java source file : ServerAction.javain the module : infrastructure

--- facets/java/org/meveo/model/customEntities/ServerAction.java
+++ facets/java/org/meveo/model/customEntities/ServerAction.java
@@ -3,9 +3,6 @@ package org.meveo.model.customEntities;
 import org.meveo.model.CustomEntity;
 import java.util.List;
 import org.meveo.model.persistence.DBStorageType;
-import org.meveo.model.customEntities.Server;
-import org.meveo.model.customEntities.ServiceProvider;
-import java.time.Instant;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class ServerAction implements CustomEntity {
@@ -22,24 +19,6 @@ public class ServerAction implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
-    private String providerSideId;
-
-    private Server server;
-
-    private ServiceProvider provider;
-
-    private String response;
-
-    private String action;
-
-    private Long progress;
-
-    private Instant creationDate;
-
-    private String responseStatus;
-
-    private Long elapsedTimeMs;
-
     @Override()
     public String getUuid() {
         return uuid;
@@ -55,78 +34,6 @@ public class ServerAction implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
-    }
-
-    public String getProviderSideId() {
-        return providerSideId;
-    }
-
-    public void setProviderSideId(String providerSideId) {
-        this.providerSideId = providerSideId;
-    }
-
-    public Server getServer() {
-        return server;
-    }
-
-    public void setServer(Server server) {
-        this.server = server;
-    }
-
-    public ServiceProvider getProvider() {
-        return provider;
-    }
-
-    public void setProvider(ServiceProvider provider) {
-        this.provider = provider;
-    }
-
-    public String getResponse() {
-        return response;
-    }
-
-    public void setResponse(String response) {
-        this.response = response;
-    }
-
-    public String getAction() {
-        return action;
-    }
-
-    public void setAction(String action) {
-        this.action = action;
-    }
-
-    public Long getProgress() {
-        return progress;
-    }
-
-    public void setProgress(Long progress) {
-        this.progress = progress;
-    }
-
-    public Instant getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(Instant creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    public String getResponseStatus() {
-        return responseStatus;
-    }
-
-    public void setResponseStatus(String responseStatus) {
-        this.responseStatus = responseStatus;
-    }
-
-    public Long getElapsedTimeMs() {
-        return elapsedTimeMs;
-    }
-
-    public void setElapsedTimeMs(Long elapsedTimeMs) {
-        this.elapsedTimeMs = elapsedTimeMs;
     }
 
     @Override()
