ad1341c13aec08f68377afc942e93b91e1985079
meveo.admin <no-mail@meveo.com>
2022-03-15 15:58:36 +0000

    Add the cet java source file : DomainName.javain the module : infrastructure

--- facets/java/org/meveo/model/customEntities/DomainName.java
+++ facets/java/org/meveo/model/customEntities/DomainName.java
@@ -3,8 +3,6 @@ package org.meveo.model.customEntities;
 import org.meveo.model.CustomEntity;
 import java.util.List;
 import org.meveo.model.persistence.DBStorageType;
-import org.meveo.model.customEntities.ServiceProvider;
-import java.time.Instant;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class DomainName implements CustomEntity {
@@ -21,20 +19,6 @@ public class DomainName implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
-    private ServiceProvider registrar;
-
-    private Instant registrationEndDate;
-
-    private Instant lastUpdate;
-
-    private String name;
-
-    private Instant registrationDate;
-
-    private Instant creationDate;
-
-    private String tld;
-
     @Override()
     public String getUuid() {
         return uuid;
@@ -50,62 +34,6 @@ public class DomainName implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
-    }
-
-    public ServiceProvider getRegistrar() {
-        return registrar;
-    }
-
-    public void setRegistrar(ServiceProvider registrar) {
-        this.registrar = registrar;
-    }
-
-    public Instant getRegistrationEndDate() {
-        return registrationEndDate;
-    }
-
-    public void setRegistrationEndDate(Instant registrationEndDate) {
-        this.registrationEndDate = registrationEndDate;
-    }
-
-    public Instant getLastUpdate() {
-        return lastUpdate;
-    }
-
-    public void setLastUpdate(Instant lastUpdate) {
-        this.lastUpdate = lastUpdate;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Instant getRegistrationDate() {
-        return registrationDate;
-    }
-
-    public void setRegistrationDate(Instant registrationDate) {
-        this.registrationDate = registrationDate;
-    }
-
-    public Instant getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(Instant creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    public String getTld() {
-        return tld;
-    }
-
-    public void setTld(String tld) {
-        this.tld = tld;
     }
 
     @Override()
