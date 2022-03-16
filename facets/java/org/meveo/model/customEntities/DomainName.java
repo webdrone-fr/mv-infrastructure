4eff47b555a288b9639a479664c0c420d8c5f915
meveo.admin <no-mail@meveo.com>
2022-03-16 07:51:49 +0000

    Add the cet java source file : DomainName.javain the module : infrastructure

--- facets/java/org/meveo/model/customEntities/DomainName.java
+++ facets/java/org/meveo/model/customEntities/DomainName.java
@@ -30,6 +30,8 @@ public class DomainName implements CustomEntity {
 
     private Instant creationDate;
 
+    private String tld;
+
     @Override()
     public String getUuid() {
         return uuid;
@@ -85,6 +87,14 @@ public class DomainName implements CustomEntity {
 
     public void setCreationDate(Instant creationDate) {
         this.creationDate = creationDate;
+    }
+
+    public String getTld() {
+        return tld;
+    }
+
+    public void setTld(String tld) {
+        this.tld = tld;
     }
 
     @Override()
