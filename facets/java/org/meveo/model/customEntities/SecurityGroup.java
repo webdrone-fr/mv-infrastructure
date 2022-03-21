6c174be110a540745cda875b4431e071959966a4
meveo.admin <no-mail@meveo.com>
2022-03-21 16:01:20 +0000

    Add the cet java source file : SecurityGroup.javain the module : infrastructure

--- facets/java/org/meveo/model/customEntities/SecurityGroup.java
+++ facets/java/org/meveo/model/customEntities/SecurityGroup.java
@@ -3,8 +3,6 @@ package org.meveo.model.customEntities;
 import org.meveo.model.CustomEntity;
 import java.util.List;
 import org.meveo.model.persistence.DBStorageType;
-import java.time.Instant;
-import java.util.ArrayList;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class SecurityGroup implements CustomEntity {
@@ -21,36 +19,6 @@ public class SecurityGroup implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
-    private Boolean enableDefaultSecurity;
-
-    private String providerSideId;
-
-    private String description;
-
-    private String project;
-
-    private Instant creationDate;
-
-    private List<String> tags = new ArrayList<>();
-
-    private Instant lastUpdated;
-
-    private List<String> servers = new ArrayList<>();
-
-    private String zone;
-
-    private String inboundDefaultPolicy;
-
-    private String outboundDefaultPolicy;
-
-    private Boolean projectDefault;
-
-    private String name;
-
-    private String state;
-
-    private Boolean stateful;
-
     @Override()
     public String getUuid() {
         return uuid;
@@ -66,126 +34,6 @@ public class SecurityGroup implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
-    }
-
-    public Boolean getEnableDefaultSecurity() {
-        return enableDefaultSecurity;
-    }
-
-    public void setEnableDefaultSecurity(Boolean enableDefaultSecurity) {
-        this.enableDefaultSecurity = enableDefaultSecurity;
-    }
-
-    public String getProviderSideId() {
-        return providerSideId;
-    }
-
-    public void setProviderSideId(String providerSideId) {
-        this.providerSideId = providerSideId;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getProject() {
-        return project;
-    }
-
-    public void setProject(String project) {
-        this.project = project;
-    }
-
-    public Instant getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(Instant creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    public List<String> getTags() {
-        return tags;
-    }
-
-    public void setTags(List<String> tags) {
-        this.tags = tags;
-    }
-
-    public Instant getLastUpdated() {
-        return lastUpdated;
-    }
-
-    public void setLastUpdated(Instant lastUpdated) {
-        this.lastUpdated = lastUpdated;
-    }
-
-    public List<String> getServers() {
-        return servers;
-    }
-
-    public void setServers(List<String> servers) {
-        this.servers = servers;
-    }
-
-    public String getZone() {
-        return zone;
-    }
-
-    public void setZone(String zone) {
-        this.zone = zone;
-    }
-
-    public String getInboundDefaultPolicy() {
-        return inboundDefaultPolicy;
-    }
-
-    public void setInboundDefaultPolicy(String inboundDefaultPolicy) {
-        this.inboundDefaultPolicy = inboundDefaultPolicy;
-    }
-
-    public String getOutboundDefaultPolicy() {
-        return outboundDefaultPolicy;
-    }
-
-    public void setOutboundDefaultPolicy(String outboundDefaultPolicy) {
-        this.outboundDefaultPolicy = outboundDefaultPolicy;
-    }
-
-    public Boolean getProjectDefault() {
-        return projectDefault;
-    }
-
-    public void setProjectDefault(Boolean projectDefault) {
-        this.projectDefault = projectDefault;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public Boolean getStateful() {
-        return stateful;
-    }
-
-    public void setStateful(Boolean stateful) {
-        this.stateful = stateful;
     }
 
     @Override()
