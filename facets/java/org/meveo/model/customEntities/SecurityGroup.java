feebf597176be1b073951724b8343218b5b289d7
meveo.admin <no-mail@meveo.com>
2022-03-22 07:50:02 +0000

    Add the cet java source file : SecurityGroup.javain the module : infrastructure

--- facets/java/org/meveo/model/customEntities/SecurityGroup.java
+++ facets/java/org/meveo/model/customEntities/SecurityGroup.java
@@ -3,8 +3,8 @@ package org.meveo.model.customEntities;
 import org.meveo.model.CustomEntity;
 import java.util.List;
 import org.meveo.model.persistence.DBStorageType;
-import java.util.ArrayList;
 import java.time.Instant;
+import java.util.ArrayList;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class SecurityGroup implements CustomEntity {
@@ -21,6 +21,16 @@ public class SecurityGroup implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
+    private Boolean enableDefaultSecurity;
+
+    private String description;
+
+    private String project;
+
+    private Instant creationDate;
+
+    private List<String> tags = new ArrayList<>();
+
     private List<String> servers = new ArrayList<>();
 
     private String zone;
@@ -33,17 +43,9 @@ public class SecurityGroup implements CustomEntity {
 
     private String name;
 
-    private String description;
-
-    private String project;
-
     private String state;
 
-    private Instant creationDate;
-
     private Boolean stateful;
-
-    private List<String> tags = new ArrayList<>();
 
     @Override()
     public String getUuid() {
@@ -60,6 +62,46 @@ public class SecurityGroup implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
+    }
+
+    public Boolean getEnableDefaultSecurity() {
+        return enableDefaultSecurity;
+    }
+
+    public void setEnableDefaultSecurity(Boolean enableDefaultSecurity) {
+        this.enableDefaultSecurity = enableDefaultSecurity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getProject() {
+        return project;
+    }
+
+    public void setProject(String project) {
+        this.project = project;
+    }
+
+    public Instant getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Instant creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
     }
 
     public List<String> getServers() {
@@ -110,22 +152,6 @@ public class SecurityGroup implements CustomEntity {
         this.name = name;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getProject() {
-        return project;
-    }
-
-    public void setProject(String project) {
-        this.project = project;
-    }
-
     public String getState() {
         return state;
     }
@@ -134,28 +160,12 @@ public class SecurityGroup implements CustomEntity {
         this.state = state;
     }
 
-    public Instant getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(Instant creationDate) {
-        this.creationDate = creationDate;
-    }
-
     public Boolean getStateful() {
         return stateful;
     }
 
     public void setStateful(Boolean stateful) {
         this.stateful = stateful;
-    }
-
-    public List<String> getTags() {
-        return tags;
-    }
-
-    public void setTags(List<String> tags) {
-        this.tags = tags;
     }
 
     @Override()
