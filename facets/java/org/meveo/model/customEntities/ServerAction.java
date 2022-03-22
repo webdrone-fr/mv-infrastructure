9865dd539ebc8cb0a948b577a316e47c8f74008a
meveo.admin <no-mail@meveo.com>
2022-03-22 07:50:19 +0000

    Add the cet java source file : ServerAction.javain the module : infrastructure

--- facets/java/org/meveo/model/customEntities/ServerAction.java
+++ facets/java/org/meveo/model/customEntities/ServerAction.java
@@ -30,6 +30,8 @@ public class ServerAction implements CustomEntity {
 
     private String action;
 
+    private Long progress;
+
     private Instant creationDate;
 
     private String responseStatus;
@@ -83,6 +85,14 @@ public class ServerAction implements CustomEntity {
 
     public void setAction(String action) {
         this.action = action;
+    }
+
+    public Long getProgress() {
+        return progress;
+    }
+
+    public void setProgress(Long progress) {
+        this.progress = progress;
     }
 
     public Instant getCreationDate() {
