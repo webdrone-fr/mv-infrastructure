f644c8d4f0c124c108e4fe2ce8862ef3dca77261
meveo.admin <no-mail@meveo.com>
2022-03-22 07:49:38 +0000

    Add the cet java source file : ApplicationInstance.javain the module : infrastructure

--- facets/java/org/meveo/model/customEntities/ApplicationInstance.java
+++ facets/java/org/meveo/model/customEntities/ApplicationInstance.java
@@ -3,7 +3,6 @@ package org.meveo.model.customEntities;
 import org.meveo.model.CustomEntity;
 import java.util.List;
 import org.meveo.model.persistence.DBStorageType;
-import java.util.ArrayList;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class ApplicationInstance implements CustomEntity {
@@ -20,18 +19,6 @@ public class ApplicationInstance implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
-    private List<String> investigationTypes = new ArrayList<>();
-
-    private String application;
-
-    private Boolean isStartServiceSource;
-
-    private Long port;
-
-    private String urlDomain;
-
-    private String repoName;
-
     @Override()
     public String getUuid() {
         return uuid;
@@ -47,54 +34,6 @@ public class ApplicationInstance implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
-    }
-
-    public List<String> getInvestigationTypes() {
-        return investigationTypes;
-    }
-
-    public void setInvestigationTypes(List<String> investigationTypes) {
-        this.investigationTypes = investigationTypes;
-    }
-
-    public String getApplication() {
-        return application;
-    }
-
-    public void setApplication(String application) {
-        this.application = application;
-    }
-
-    public Boolean getIsStartServiceSource() {
-        return isStartServiceSource;
-    }
-
-    public void setIsStartServiceSource(Boolean isStartServiceSource) {
-        this.isStartServiceSource = isStartServiceSource;
-    }
-
-    public Long getPort() {
-        return port;
-    }
-
-    public void setPort(Long port) {
-        this.port = port;
-    }
-
-    public String getUrlDomain() {
-        return urlDomain;
-    }
-
-    public void setUrlDomain(String urlDomain) {
-        this.urlDomain = urlDomain;
-    }
-
-    public String getRepoName() {
-        return repoName;
-    }
-
-    public void setRepoName(String repoName) {
-        this.repoName = repoName;
     }
 
     @Override()
