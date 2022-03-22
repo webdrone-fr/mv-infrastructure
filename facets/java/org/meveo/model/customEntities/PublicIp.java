89ee7a3dbfbe8a4b617f4c5f4dbebd3628d1b771
meveo.admin <no-mail@meveo.com>
2022-03-22 07:49:41 +0000

    Add the cet java source file : PublicIp.javain the module : infrastructure

--- facets/java/org/meveo/model/customEntities/PublicIp.java
+++ facets/java/org/meveo/model/customEntities/PublicIp.java
@@ -3,9 +3,6 @@ package org.meveo.model.customEntities;
 import org.meveo.model.CustomEntity;
 import java.util.List;
 import org.meveo.model.persistence.DBStorageType;
-import org.meveo.model.customEntities.Server;
-import org.meveo.model.customEntities.ServiceProvider;
-import java.util.ArrayList;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class PublicIp implements CustomEntity {
@@ -22,28 +19,6 @@ public class PublicIp implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
-    private String providerSideId;
-
-    private Server server;
-
-    private ServiceProvider provider;
-
-    private String zone;
-
-    private String organization;
-
-    private String project;
-
-    private String ipVFourAddress;
-
-    private String location;
-
-    private String locationDefinition;
-
-    private String reverse;
-
-    private List<String> tags = new ArrayList<>();
-
     @Override()
     public String getUuid() {
         return uuid;
@@ -59,94 +34,6 @@ public class PublicIp implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
-    }
-
-    public String getProviderSideId() {
-        return providerSideId;
-    }
-
-    public void setProviderSideId(String providerSideId) {
-        this.providerSideId = providerSideId;
-    }
-
-    public Server getServer() {
-        return server;
-    }
-
-    public void setServer(Server server) {
-        this.server = server;
-    }
-
-    public ServiceProvider getProvider() {
-        return provider;
-    }
-
-    public void setProvider(ServiceProvider provider) {
-        this.provider = provider;
-    }
-
-    public String getZone() {
-        return zone;
-    }
-
-    public void setZone(String zone) {
-        this.zone = zone;
-    }
-
-    public String getOrganization() {
-        return organization;
-    }
-
-    public void setOrganization(String organization) {
-        this.organization = organization;
-    }
-
-    public String getProject() {
-        return project;
-    }
-
-    public void setProject(String project) {
-        this.project = project;
-    }
-
-    public String getIpVFourAddress() {
-        return ipVFourAddress;
-    }
-
-    public void setIpVFourAddress(String ipVFourAddress) {
-        this.ipVFourAddress = ipVFourAddress;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public String getLocationDefinition() {
-        return locationDefinition;
-    }
-
-    public void setLocationDefinition(String locationDefinition) {
-        this.locationDefinition = locationDefinition;
-    }
-
-    public String getReverse() {
-        return reverse;
-    }
-
-    public void setReverse(String reverse) {
-        this.reverse = reverse;
-    }
-
-    public List<String> getTags() {
-        return tags;
-    }
-
-    public void setTags(List<String> tags) {
-        this.tags = tags;
     }
 
     @Override()
