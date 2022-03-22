7dbd63de4bf45c8bc053c4327749b0fa082197ae
meveo.admin <no-mail@meveo.com>
2022-03-22 07:49:41 +0000

    Add the cet java source file : ServerUserData.javain the module : infrastructure

--- facets/java/org/meveo/model/customEntities/ServerUserData.java
+++ facets/java/org/meveo/model/customEntities/ServerUserData.java
@@ -3,7 +3,6 @@ package org.meveo.model.customEntities;
 import org.meveo.model.CustomEntity;
 import java.util.List;
 import org.meveo.model.persistence.DBStorageType;
-import org.meveo.model.customEntities.Server;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class ServerUserData implements CustomEntity {
@@ -20,18 +19,6 @@ public class ServerUserData implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
-    private Server server;
-
-    private String zone;
-
-    private String name;
-
-    private String contentType;
-
-    private String content;
-
-    private String serverSideKey;
-
     @Override()
     public String getUuid() {
         return uuid;
@@ -47,54 +34,6 @@ public class ServerUserData implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
-    }
-
-    public Server getServer() {
-        return server;
-    }
-
-    public void setServer(Server server) {
-        this.server = server;
-    }
-
-    public String getZone() {
-        return zone;
-    }
-
-    public void setZone(String zone) {
-        this.zone = zone;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getContentType() {
-        return contentType;
-    }
-
-    public void setContentType(String contentType) {
-        this.contentType = contentType;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getServerSideKey() {
-        return serverSideKey;
-    }
-
-    public void setServerSideKey(String serverSideKey) {
-        this.serverSideKey = serverSideKey;
     }
 
     @Override()
