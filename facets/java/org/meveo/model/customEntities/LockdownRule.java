359e902777f58f9f8bdf85144021b77d2f175cfc
meveo.admin <no-mail@meveo.com>
2022-03-22 07:49:39 +0000

    Add the cet java source file : LockdownRule.javain the module : infrastructure

--- facets/java/org/meveo/model/customEntities/LockdownRule.java
+++ facets/java/org/meveo/model/customEntities/LockdownRule.java
@@ -3,9 +3,6 @@ package org.meveo.model.customEntities;
 import org.meveo.model.CustomEntity;
 import java.util.List;
 import org.meveo.model.persistence.DBStorageType;
-import java.util.ArrayList;
-import java.time.Instant;
-import org.meveo.model.customEntities.DomainName;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class LockdownRule implements CustomEntity {
@@ -22,24 +19,6 @@ public class LockdownRule implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
-    private List<String> ipRanges = new ArrayList<>();
-
-    private Instant lastUpdated;
-
-    private String providerSideId;
-
-    private Boolean paused;
-
-    private List<String> urls = new ArrayList<>();
-
-    private DomainName domainName;
-
-    private String description;
-
-    private Instant creationDate;
-
-    private List<String> ips = new ArrayList<>();
-
     @Override()
     public String getUuid() {
         return uuid;
@@ -55,78 +34,6 @@ public class LockdownRule implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
-    }
-
-    public List<String> getIpRanges() {
-        return ipRanges;
-    }
-
-    public void setIpRanges(List<String> ipRanges) {
-        this.ipRanges = ipRanges;
-    }
-
-    public Instant getLastUpdated() {
-        return lastUpdated;
-    }
-
-    public void setLastUpdated(Instant lastUpdated) {
-        this.lastUpdated = lastUpdated;
-    }
-
-    public String getProviderSideId() {
-        return providerSideId;
-    }
-
-    public void setProviderSideId(String providerSideId) {
-        this.providerSideId = providerSideId;
-    }
-
-    public Boolean getPaused() {
-        return paused;
-    }
-
-    public void setPaused(Boolean paused) {
-        this.paused = paused;
-    }
-
-    public List<String> getUrls() {
-        return urls;
-    }
-
-    public void setUrls(List<String> urls) {
-        this.urls = urls;
-    }
-
-    public DomainName getDomainName() {
-        return domainName;
-    }
-
-    public void setDomainName(DomainName domainName) {
-        this.domainName = domainName;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Instant getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(Instant creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    public List<String> getIps() {
-        return ips;
-    }
-
-    public void setIps(List<String> ips) {
-        this.ips = ips;
     }
 
     @Override()
