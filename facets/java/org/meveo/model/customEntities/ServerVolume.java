2d7636c654fe4d30f84abbf936ae4cb2a729bcf6
meveo.admin <no-mail@meveo.com>
2022-03-22 07:49:38 +0000

    Add the cet java source file : ServerVolume.javain the module : infrastructure

--- facets/java/org/meveo/model/customEntities/ServerVolume.java
+++ facets/java/org/meveo/model/customEntities/ServerVolume.java
@@ -3,7 +3,6 @@ package org.meveo.model.customEntities;
 import org.meveo.model.CustomEntity;
 import java.util.List;
 import org.meveo.model.persistence.DBStorageType;
-import java.time.Instant;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class ServerVolume implements CustomEntity {
@@ -20,28 +19,6 @@ public class ServerVolume implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
-    private Instant lastUpdated;
-
-    private String providerSideId;
-
-    private String server;
-
-    private String volumeType;
-
-    private String size;
-
-    private String zone;
-
-    private String name;
-
-    private String state;
-
-    private String baseVolume;
-
-    private Instant creationDate;
-
-    private Boolean isBoot;
-
     @Override()
     public String getUuid() {
         return uuid;
@@ -57,94 +34,6 @@ public class ServerVolume implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
-    }
-
-    public Instant getLastUpdated() {
-        return lastUpdated;
-    }
-
-    public void setLastUpdated(Instant lastUpdated) {
-        this.lastUpdated = lastUpdated;
-    }
-
-    public String getProviderSideId() {
-        return providerSideId;
-    }
-
-    public void setProviderSideId(String providerSideId) {
-        this.providerSideId = providerSideId;
-    }
-
-    public String getServer() {
-        return server;
-    }
-
-    public void setServer(String server) {
-        this.server = server;
-    }
-
-    public String getVolumeType() {
-        return volumeType;
-    }
-
-    public void setVolumeType(String volumeType) {
-        this.volumeType = volumeType;
-    }
-
-    public String getSize() {
-        return size;
-    }
-
-    public void setSize(String size) {
-        this.size = size;
-    }
-
-    public String getZone() {
-        return zone;
-    }
-
-    public void setZone(String zone) {
-        this.zone = zone;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getBaseVolume() {
-        return baseVolume;
-    }
-
-    public void setBaseVolume(String baseVolume) {
-        this.baseVolume = baseVolume;
-    }
-
-    public Instant getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(Instant creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    public Boolean getIsBoot() {
-        return isBoot;
-    }
-
-    public void setIsBoot(Boolean isBoot) {
-        this.isBoot = isBoot;
     }
 
     @Override()
