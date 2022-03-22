d13eca3bd50912e8c50daa0827bae40ec0d68fb7
meveo.admin <no-mail@meveo.com>
2022-03-22 07:49:42 +0000

    Add the cet java source file : DnsRecord.javain the module : infrastructure

--- facets/java/org/meveo/model/customEntities/DnsRecord.java
+++ facets/java/org/meveo/model/customEntities/DnsRecord.java
@@ -3,8 +3,6 @@ package org.meveo.model.customEntities;
 import org.meveo.model.CustomEntity;
 import java.util.List;
 import org.meveo.model.persistence.DBStorageType;
-import org.meveo.model.customEntities.DomainName;
-import java.time.Instant;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class DnsRecord implements CustomEntity {
@@ -21,30 +19,6 @@ public class DnsRecord implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
-    private String providerSideId;
-
-    private String recordType;
-
-    private DomainName domainName;
-
-    private Boolean isLocked;
-
-    private String name;
-
-    private Instant lastSyncDate;
-
-    private Boolean proxied;
-
-    private Instant creationDate;
-
-    private Long priority;
-
-    private Boolean proxiable;
-
-    private Long ttl;
-
-    private String value;
-
     @Override()
     public String getUuid() {
         return uuid;
@@ -60,102 +34,6 @@ public class DnsRecord implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
-    }
-
-    public String getProviderSideId() {
-        return providerSideId;
-    }
-
-    public void setProviderSideId(String providerSideId) {
-        this.providerSideId = providerSideId;
-    }
-
-    public String getRecordType() {
-        return recordType;
-    }
-
-    public void setRecordType(String recordType) {
-        this.recordType = recordType;
-    }
-
-    public DomainName getDomainName() {
-        return domainName;
-    }
-
-    public void setDomainName(DomainName domainName) {
-        this.domainName = domainName;
-    }
-
-    public Boolean getIsLocked() {
-        return isLocked;
-    }
-
-    public void setIsLocked(Boolean isLocked) {
-        this.isLocked = isLocked;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Instant getLastSyncDate() {
-        return lastSyncDate;
-    }
-
-    public void setLastSyncDate(Instant lastSyncDate) {
-        this.lastSyncDate = lastSyncDate;
-    }
-
-    public Boolean getProxied() {
-        return proxied;
-    }
-
-    public void setProxied(Boolean proxied) {
-        this.proxied = proxied;
-    }
-
-    public Instant getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(Instant creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    public Long getPriority() {
-        return priority;
-    }
-
-    public void setPriority(Long priority) {
-        this.priority = priority;
-    }
-
-    public Boolean getProxiable() {
-        return proxiable;
-    }
-
-    public void setProxiable(Boolean proxiable) {
-        this.proxiable = proxiable;
-    }
-
-    public Long getTtl() {
-        return ttl;
-    }
-
-    public void setTtl(Long ttl) {
-        this.ttl = ttl;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
     }
 
     @Override()
