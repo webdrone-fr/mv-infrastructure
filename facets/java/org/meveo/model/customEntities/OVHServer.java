112a4857a92794986abdbcb1a6b57c43cf0cfae0
meveo.admin <no-mail@meveo.com>
2022-03-22 07:49:43 +0000

    Add the cet java source file : OVHServer.javain the module : infrastructure

--- facets/java/org/meveo/model/customEntities/OVHServer.java
+++ facets/java/org/meveo/model/customEntities/OVHServer.java
@@ -3,8 +3,6 @@ package org.meveo.model.customEntities;
 import org.meveo.model.CustomEntity;
 import java.util.List;
 import org.meveo.model.persistence.DBStorageType;
-import org.meveo.model.customEntities.ServerNetwork;
-import java.util.ArrayList;
 import org.meveo.model.customEntities.Server;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -22,10 +20,6 @@ public class OVHServer extends Server implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
-    private String keyName;
-
-    private List<ServerNetwork> network = new ArrayList<>();
-
     @Override()
     public String getUuid() {
         return uuid;
@@ -41,22 +35,6 @@ public class OVHServer extends Server implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
-    }
-
-    public String getKeyName() {
-        return keyName;
-    }
-
-    public void setKeyName(String keyName) {
-        this.keyName = keyName;
-    }
-
-    public List<ServerNetwork> getNetwork() {
-        return network;
-    }
-
-    public void setNetwork(List<ServerNetwork> network) {
-        this.network = network;
     }
 
     @Override()
