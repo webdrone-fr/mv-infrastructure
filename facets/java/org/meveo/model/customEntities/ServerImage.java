f644d3915a1964b766f65374a3fc1e7092478d0e
meveo.admin <no-mail@meveo.com>
2022-03-22 07:49:39 +0000

    Add the cet java source file : ServerImage.javain the module : infrastructure

--- facets/java/org/meveo/model/customEntities/ServerImage.java
+++ facets/java/org/meveo/model/customEntities/ServerImage.java
@@ -3,12 +3,6 @@ package org.meveo.model.customEntities;
 import org.meveo.model.CustomEntity;
 import java.util.List;
 import org.meveo.model.persistence.DBStorageType;
-import java.util.Map;
-import org.meveo.model.customEntities.ServerVolume;
-import java.util.HashMap;
-import java.time.Instant;
-import java.util.ArrayList;
-import org.meveo.model.customEntities.Bootscript;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class ServerImage implements CustomEntity {
@@ -25,36 +19,6 @@ public class ServerImage implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
-    private String fromServer;
-
-    private String providerSideId;
-
-    private Map<String, ServerVolume> additionalVolumes = new HashMap<>();
-
-    private String project;
-
-    private Instant creationDate;
-
-    private List<String> tags = new ArrayList<>();
-
-    private Instant lastUpdated;
-
-    private String zone;
-
-    private Bootscript defaultBootscript;
-
-    private String organization;
-
-    private String name;
-
-    private Boolean isPublic;
-
-    private ServerVolume rootVolume;
-
-    private String arch;
-
-    private String state;
-
     @Override()
     public String getUuid() {
         return uuid;
@@ -70,126 +34,6 @@ public class ServerImage implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
-    }
-
-    public String getFromServer() {
-        return fromServer;
-    }
-
-    public void setFromServer(String fromServer) {
-        this.fromServer = fromServer;
-    }
-
-    public String getProviderSideId() {
-        return providerSideId;
-    }
-
-    public void setProviderSideId(String providerSideId) {
-        this.providerSideId = providerSideId;
-    }
-
-    public Map<String, ServerVolume> getAdditionalVolumes() {
-        return additionalVolumes;
-    }
-
-    public void setAdditionalVolumes(Map<String, ServerVolume> additionalVolumes) {
-        this.additionalVolumes = additionalVolumes;
-    }
-
-    public String getProject() {
-        return project;
-    }
-
-    public void setProject(String project) {
-        this.project = project;
-    }
-
-    public Instant getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(Instant creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    public List<String> getTags() {
-        return tags;
-    }
-
-    public void setTags(List<String> tags) {
-        this.tags = tags;
-    }
-
-    public Instant getLastUpdated() {
-        return lastUpdated;
-    }
-
-    public void setLastUpdated(Instant lastUpdated) {
-        this.lastUpdated = lastUpdated;
-    }
-
-    public String getZone() {
-        return zone;
-    }
-
-    public void setZone(String zone) {
-        this.zone = zone;
-    }
-
-    public Bootscript getDefaultBootscript() {
-        return defaultBootscript;
-    }
-
-    public void setDefaultBootscript(Bootscript defaultBootscript) {
-        this.defaultBootscript = defaultBootscript;
-    }
-
-    public String getOrganization() {
-        return organization;
-    }
-
-    public void setOrganization(String organization) {
-        this.organization = organization;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Boolean getIsPublic() {
-        return isPublic;
-    }
-
-    public void setIsPublic(Boolean isPublic) {
-        this.isPublic = isPublic;
-    }
-
-    public ServerVolume getRootVolume() {
-        return rootVolume;
-    }
-
-    public void setRootVolume(ServerVolume rootVolume) {
-        this.rootVolume = rootVolume;
-    }
-
-    public String getArch() {
-        return arch;
-    }
-
-    public void setArch(String arch) {
-        this.arch = arch;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
     }
 
     @Override()
